bd537f0a0d1c0a9b39bfdea93d660eec2b96308b
Robert Mitchell <robert.mitchell@vualto.com>
2017-06-08 18:12:05 +0100

    added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function (grunt) {
+    var jwplayerUrl = "https://cdn.vuplay.co.uk/jwplayer/7.11.3/jwplayer.js";
+
+    grunt.initConfig({
+        package: grunt.file.readJSON("package.json"),
+
+        clean: ["dist/*"],
+        
+        copy: {
+            all: {
+                expand: true,
+                src: ["index.html"],
+                dest: "dist/",
+                flatten: true
+            }
+        },
+        
+        concat: {
+            options: {},
+            dist: {
+                src: [
+                    "src/vuplay.js"
+                ],
+                dest: "dist/vuplay.js"
+            }
+        },
+        
+        uglify: {
+            js: {
+                files: {
+                    "dist/vuplay.min.js": ["dist/vuplay.js"]
+                }
+            }
+        },
+        
+        "string-replace": {
+            dist: {
+                files: [
+                    {
+                        src: "dist/index.html",
+                        dest: "dist/index.html"
+                    }
+                ],
+                options: {
+                    replacements: [
+                        {
+                            pattern: "{jwplayer}",
+                            replacement: jwplayerUrl
+                        },
+                        {
+                            pattern: "{vuplayjs}",
+                            replacement: grunt.option("debug") ? "vuplay.js" : "vuplay.min.js"
+                        }
+                    ]
+                }
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    protocol: "https",
+                    hostname: "jwplayer.vuplay.local.drm.technology",
+                    port: 14703,
+                    base: "dist",
+                    keepalive: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks("grunt-string-replace");
+    grunt.loadNpmTasks("grunt-contrib-connect");
+
+    grunt.registerTask("build", ["clean", "copy", "concat", "uglify", "string-replace"]);
+    grunt.registerTask("serve", ["build", "connect"]);
+}
