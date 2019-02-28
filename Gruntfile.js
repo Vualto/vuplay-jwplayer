4871e18fa0fa1d7fa6c521e5db8668d6aea323aa
Lorenzo Koundouris <lorenzo.koundouris@vualto.com>
2019-02-28 17:13:29 +0000

    apply updates inline with other demo pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,41 @@
-module.exports = function (grunt) {
-    var jwplayerUrl = "https://cdn.vuplay.co.uk/jwplayer/8.5.6/jwplayer.js";
-
+module.exports = function(grunt) {
     grunt.initConfig({
-        package: grunt.file.readJSON("package.json"),
+        dist: "dist",
 
-        clean: ["dist/*"],
-        
+        pkg: grunt.file.readJSON("package.json"),
+
+        clean: ["<%= dist %>/*"],
+
+        concat: {
+            options: {
+                banner: "// <%= pkg.description %> \n",
+            },
+            dist: {
+                src: ["src/<%= pkg.name %>.js"],
+                dest: "dist/<%= pkg.name %>.js",
+            },
+        },
+
         copy: {
             all: {
                 expand: true,
-                src: ["index.html"],
-                dest: "dist/",
-                flatten: true
-            }
+                src: ["index.html", "assets/vuplay_poster.png"],
+                dest: "<%= dist %>/",
+                flatten: true,
+            },
         },
-        
-        concat: {
-            options: {},
-            dist: {
-                src: [
-                    "vuplay.js"
-                ],
-                dest: "dist/vuplay.js"
-            }
-        },
-        
-        uglify: {
-            js: {
-                files: {
-                    "dist/vuplay.min.js": ["dist/vuplay.js"]
-                }
-            }
-        },
-        
-        "string-replace": {
-            dist: {
-                files: [
-                    {
-                        src: "dist/index.html",
-                        dest: "dist/index.html"
-                    }
-                ],
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            scripts: {
+                files: ["**/*.js", "./index.html"],
+                tasks: ["build"],
                 options: {
-                    replacements: [
-                        {
-                            pattern: "{jwplayer}",
-                            replacement: jwplayerUrl
-                        },
-                        {
-                            pattern: "{jwplayer-key}",
-                            replacement: grunt.option("jwplayer-key")
-                        },
-                        {
-                            pattern: "{vuplayjs}",
-                            replacement: grunt.option("debug") ? "vuplay.js" : "vuplay.min.js"
-                        }
-                    ]
-                }
-            }
+                    spawn: false,
+                },
+            },
         },
 
         connect: {
@@ -67,19 +45,28 @@ module.exports = function (grunt) {
                     hostname: "jwplayer.vuplay.local.drm.technology",
                     port: 14705,
                     base: "dist",
-                    keepalive: true
-                }
-            }
-        }
+                    keepalive: true,
+                },
+            },
+        },
+
+        concurrent: {
+            connectandwatch: {
+                tasks: ["connect", "watch"],
+                options: {
+                    logConcurrentOutput: true,
+                },
+            },
+        },
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-concat");
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks("grunt-string-replace");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-concurrent");
 
-    grunt.registerTask("build", ["clean", "copy", "concat", "uglify", "string-replace"]);
-    grunt.registerTask("serve", ["build", "connect"]);
-}
+    grunt.registerTask("build", ["clean", "concat", "copy"]);
+    grunt.registerTask("serve", ["build", "concurrent:connectandwatch"]);
+};
