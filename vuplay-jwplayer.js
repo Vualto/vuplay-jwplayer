f5c386257c156216463d841699aeb99f1c42ff48
somerobmitchell <robert.mitchell@vualto.com>
2022-02-22 10:19:53 +0000

    use renewal token

--- vuplay-jwplayer.js
+++ vuplay-jwplayer.js
@@ -8,7 +8,7 @@
     // var fairplayCertURL = "https://fairplay-license.vudrm.tech/certificate/vualto-demo";
 
     // Please login to https://admin.vudrm.tech to generate a VUDRM token.
-    var vudrmToken = "vualto-demo|2022-02-18T13:50:40Z|v2|WdN04P3cGLhXVMP32EdI4g0/lY3f0GEzQbo4gngGVqxeQv0zTO7zNgMBq7LyV/0KkT8O84Y5wAJjEITGjqlmhg==|233ba637b81f241553b08a7d44bfa59c7c9a1ef201b2a824482d592c70b9fb08";
+    var vudrmToken = "vualto-demo|2022-02-21T17:21:35Z|v2|TRo81vIXkMhtEe+sUyzr8qbLn5M0OWO2m93+ev5sDi9cv04YPdl0npxWXu3E4cRyc38NwbMK9ZUJiAm5Fxcpgw==|424f6662ce7f6793a0ba93a480758a78c99228aef85bf806bf394103a23ae172";
     
     // setup jwplayer, passing the stream URLs and DRM configurations.  
     jwplayer("vuplay-container").setup({
