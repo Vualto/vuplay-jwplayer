15f2ed2b0515a947ff563af348ccfa468855db8a
Ben Godby <ben.godby@vualto.com>
2022-07-01 10:17:45 +0100

    missed a url change

--- studiodrm-jwplayer.js
+++ studiodrm-jwplayer.js
@@ -7,7 +7,7 @@
     // Set the URL to retrieve the fairplay certificate from.
     var fairplayCertURL = "<fairplay-cert-url>";
 
-    // Please refer to the following documentation for guidance on generating a Studio DRM token: https://developer.jwplayer.com/jwplayer/docs/studio-drm-token-v2
+    // Please refer to the following documentation for guidance on generating a Studio DRM token: https://developer.jwplayer.com/jwplayer/docs/studio-drm-token-api-v2
     var studioDrmToken = "<your-studiodrm-token>";
     
     // setup jwplayer, passing the stream URLs and DRM configurations.
